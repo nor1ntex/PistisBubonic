68ba46deaed80c37867d3e22c70b0b94f08e0bb7
nor1ntex <140723135+nor1ntex@users.noreply.github.com>
2024-10-24 21:00:22 -0300

    SilkRoute

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é C#?',
+    'O C# é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Etiópia?',
+    'A capital da Etiópia é Addis Ababa'
+)
+
+criaCartao(
+    'Programação',
+    'O que é delay?',
+    'Delay é um código que faz a espera dentre um comando e e outro'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz sim em Inglês?',
+    'Sim em ingles é YES (Iés)'
+)
